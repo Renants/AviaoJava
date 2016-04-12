526a42dd2370aba40cd6d9a4732c1590214a5bb1
Renants <renannojosa@gmail.com>
2016-04-12 13:25:36 -0300

    Update Airplane.java

--- src/Interfaces/Airplane.java
+++ src/Interfaces/Airplane.java
@@ -2,15 +2,14 @@ package Interfaces;
 
 public interface Airplane {
 
-	public abstract void	decolar();
+	public abstract void	decolar(); 
 	public abstract void	pousar();
-	public abstract void	acelerar();
+	public abstract void	acelerar(int velocidade);
+	public abstract void	acelerar(int velocidade,boolean turbo );
 	public abstract void	setSpeed(int v );
 	public abstract int		getSpeed();
-	public abstract void	defAlterarvoo();
 	public abstract void	alterarVelocidade();
-	public abstract float	retVelocidade(); 	
-	
+		
 	@Override
     public String toString();
 }
