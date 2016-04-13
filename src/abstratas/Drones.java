afb445fd5258e20ac6567c1d116030b5f5c2e525
Renants <renannojosa@gmail.com>
2016-04-13 15:47:40 -0300

    Update Drones.java

--- src/abstratas/Drones.java
+++ src/abstratas/Drones.java
@@ -4,7 +4,11 @@ import interfaces.Airplane;
 public abstract class Drones implements Airplane {
 	protected int		velocidade;
 	protected int		autonomia;
+	protected int		velocidademax;
+	protected int		velocidademin;
+	protected int		autonomia;
 	protected int		projeteis;
+	protected int		misseis;
 	protected boolean	gps;
 	protected boolean	noar;
 	
