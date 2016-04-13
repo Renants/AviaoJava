5215fa2c974c4240b24ae02f76c2d1dc95e235a7
Renants <renannojosa@gmail.com>
2016-04-13 15:30:46 -0300

    Update JatoMilitar.java

--- src/concretas/JatoMilitar.java
+++ src/concretas/JatoMilitar.java
@@ -6,7 +6,7 @@ public class JatoMilitar extends Jatos {
 	protected int projeteis;
 	protected int misseis;
 	protected boolean turbo;
-	protected boolean rebastecimento;
+	protected boolean sensor;
 	protected ArrayList<String> tripulantes;
 	
 	public JatoMilitar(){
@@ -30,4 +30,23 @@ public class JatoMilitar extends Jatos {
 		else System.out.println("erro velocidade incorreta");
 		} 
 	
+	public void reabastecer(){
+    	if ( noar == false && sensor == false ){
+    	System.out.println("Erro nos sensores de reabastecimento ")
+    	}else{
+       
+        System.out.println("Avião de reabastecimento acoplado ao " );
+        System.out.println("Reabastecimento do em andamento...");
+        System.out.println( "Autonomia anterior: "this.autonomia );
+        this.autonomia = this.autonomia + 5;
+        System.out.println("Autonomia Atual: " this->autonomia); }
+}
+	
+	public void reabastecimentoAerio(){
+	while( this.velocidade > velocidade - (velocidade/3) ){
+	System.out.println("reduza a velocidade em 1/3");
+	acelerar()
+	sensor = true;
+	reabastecer();}
+
 }
